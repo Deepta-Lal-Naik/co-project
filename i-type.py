b3f9291d493ab07230b21388d71060bd3ac871a0
Deepta <deepta25170@iiitd.ac.in>
2026-03-04 15:55:34 +0000

    Added function to load assembly data to program

--- i-type.py
+++ i-type.py
@@ -1,4 +1,21 @@
-instr = { "lw"  :["010","0000011"],
-          "addi" :["000","0010011"],
-          "sltiu":["011","0010011"],
-          "jalr" :["000","1100111"]  }
+#def itype(command):
+instr = { "lw" :["010","0000011"], #0-index: funct3,  1-index: opcode
+         "addi" :["000","0010011"],
+         "sltiu":["011","0010011"],
+         "jalr" :["000","1100111"]  }
+
+# Load instructions to program
+S = []
+def load(): 
+        with open("assembly.txt",'r') as fin:
+            L = fin.readlines()
+            for i in L:
+                j = i.replace(",","").split()
+                S.append(j)
+            print(S)    
+load()
+
+
+              
+
+
