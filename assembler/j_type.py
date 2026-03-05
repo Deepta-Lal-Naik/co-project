6a9a081b0e7fd687e28d6dccd9ac9a1d94428ee3
krrish-cyber6 <krrish25276@iiitd.ac.in>
2026-03-05 11:18:29 +0000

    Assembler.py with j and r type linked

--- assembler/j_type.py
+++ assembler/j_type.py
@@ -1,23 +1,46 @@
 from store import Register_Mapping
 
-def j_to_bin(instruction):
+def j_to_bin(instruction,pc,label_adrr):
 
     l = instruction.split(" ")
     l1 = l[1].split(",")
     command = l[0]
     if command!="jal":
-        print("Wrong command")
-        pass
-    if int(l1[1][:])%2==1:
-        print("Odd immediate")
-        pass
+        raise Exception
 
     rd = Register_Mapping[l1[0]]
-    offset = format(int(l1[1][:])>>1,"021b")
+    offset = label_adrr-pc
+    if offset<0:
+        offset=not(offset)
+        offset+=1
+    offset = format(offset>>1,"021b")
+
+    print(offset)
     imm20 = offset[0]
     imm10_1 = offset[10:20]
     imm11 = offset[9]
     imm19_12 = offset[1:9]
-    print(imm20+imm10_1+imm11+imm19_12+rd+"1101111")
+
     return imm20+imm10_1+imm11+imm19_12+rd+"1101111"
-j_to_bin()
+
+def j_to_bin(instruction,imm):
+
+    l = instruction.split(" ")
+    l1 = l[1].split(",")
+    command = l[0]
+    if command!="jal":
+        raise Exception
+
+    rd = Register_Mapping[l1[0]]
+    offset = imm
+    if offset<0:
+        offset=not(offset)
+        offset+=1
+    offset = format(offset>>1,"021b")
+
+    print(offset)
+    imm20 = offset[0]
+    imm10_1 = offset[10:20]
+    imm11 = offset[9]
+    imm19_12 = offset[1:9]
+    return imm20+imm10_1+imm11+imm19_12+rd+"1101111"
---
+++ co_2026_evaluation_framework_release/SimpleAssembler/Assembler.py
@@ -0,0 +1,37 @@
+import sys
+from r_type import r_to_bin
+from j_type import j_to_bin
+def createLabels(data):
+    d={}
+    pc=0
+    for i in data:
+        if ":" in i:
+            temp = i.split(":")
+            d[temp[0]]=pc
+        pc+=4
+    
+    return d
+            
+
+data_file = sys.argv[1]
+output_file = sys.argv[2]
+with open(data_file,"r") as f:
+    data = f.readlines()
+pc=0
+labels = createLabels(data)
+operations = ["add","sub","sll","slt","sltu","xor","srl","sra","or","and","jal"]
+for i in data:
+    temp = i.split(" ")
+    if temp[0] in operations[0:10]:
+        wdata = r_to_bin(i)
+
+    elif temp[0]==operations[10]:
+        label = temp[1].split(",")[1]
+        if label in labels:
+            wdata = j_to_bin(i,pc,labels[label])
+        else:
+            wdata = j_to_bin(i,int(label))
+    
+
+
+
