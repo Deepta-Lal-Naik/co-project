5bd90a058e804d215613090e0aef084f8b8e57be
krrish-cyber6 <krrish25276@iiitd.ac.in>
2026-03-06 19:06:15 +0000

    Fixed all the bugs and added the main function and init entry point, This version WORKSgit add .

--- assembler/b_type.py
+++ assembler/b_type.py
@@ -1,15 +1,12 @@
-from store import Register_Mapping,B_Type
+from store import Register_Mapping,B_Type,twos
 def b_type_encoder(func,r1,r2,off_val,pc,labels):
     b_func3=B_Type
     opcode="1100011"
-    if off_val.lstrip("-").isdigit():
+    if off_val.isdigit():
         offset=int(off_val)
     else:
         offset=labels[off_val]-pc
-    offset = offset >> 1
-    if offset<0:
-        offset=(1<<12)+offset
-    imm=format(offset,"012b")
+    imm = twos(offset>>1,12)
     imm_12=imm[0]
     imm10_5=imm[2:8]
     imm4_1=imm[8:12]
@@ -19,8 +16,6 @@ def b_type_encoder(func,r1,r2,off_val,pc,labels):
     return imm_12+imm10_5+r2+r1+b_func3[func]+imm4_1+imm_11+opcode
 
 def b_to_bin(ins,pc,labels):
-        if ":" in ins:
-            ins = ins.split(":")[1].strip()
         ins=ins.replace(",", " ")
         a=ins.split()
         return b_type_encoder(a[0],a[1],a[2],a[3],pc,labels)
